e87c146ad5ab304fd0243209c5a6be47a663cc0c
mairis <mairiscastroflorez@gmail.com>
2021-07-20 19:12:53 -0500

    commit

--- src/main.js
+++ src/main.js
@@ -4,3 +4,40 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+const btn = document.querySelector('#btnNav');
+btn.addEventListener ('click', () => {
+    if (document.getElementById('btnNav')) {
+        let modal = document.getElementById('VerModal');
+        let span = document.getElementsByClassName('close')[0];
+        let body = document.getElementsByTagName('body')[0];
+
+        btn.onclick = function() {
+            modal.style.display = 'block';
+
+            body.style.position = 'static';
+            body.style.height = '100%';
+            body.style.overflow = 'hidden';
+        }
+
+        span.onclick = function() {
+            modal.style.display = 'none';
+
+            body.style.position = 'inherit';
+            body.style.height = 'auto';
+            body.style.overflow = 'visible';
+
+        }
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = 'none';
+
+                body.style.position = 'inherit';
+                body.style.height = 'auto';
+                body.style.overflow = 'visible';
+            }
+        } 
+    }
+}
+)
