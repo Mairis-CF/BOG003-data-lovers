9245774389990b66750ad8781937ec5898a200cc
mairis <mairiscastroflorez@gmail.com>
2021-07-28 07:28:09 -0500

    Aplicando estilos y función mostrardata

--- src/data/ghibli/ghibli.js
+++ src/data/ghibli/ghibli.js
@@ -679,7 +679,7 @@ export default {
     },
     {
       "id": "ebbb6b7c-945c-41ee-a792-de0e43191bd8",
-      "title": "Porco Rosso",
+     "title": "Porco Rosso",
       "description": "Porco Rosso, known in Japan as Crimson Pig (Kurenai no Buta) is the sixth animated film by Hayao Miyazaki and released in 1992. You're introduced to an Italian World War I fighter ace, now living as a freelance bounty hunter chasing 'air pirates' in the Adriatic Sea. He has been given a curse that changed his head to that of a pig. Once called Marco Pagot, he is now known to the world as 'Porco Rosso', Italian for 'Red Pig.'",
       "director": "Hayao Miyazaki",
       "producer": "Toshio Suzuki",
--- src/main.js
+++ src/main.js
@@ -1,31 +1,53 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+
 import data from './data/ghibli/ghibli.js';
 
-
-console.log(example, data);
-
-//ejemplo de filter y map
-
 const peliculas = data.films;
-console.table (peliculas);
+console.table(peliculas);
 
-function filtrarNombres (title) {
+
+/*ejemplo de filter y map
+
+const nombres = function filtrarNombres (title) {
     let titulosPeliculas = ''; 
-    titulosPeliculas = data.films.filter(movie => movie.director === title);
+    titulosPeliculas = data.films.filter(movie => movie.poster === title);
 
    // titulosPeliculas = data.films.filter(movie => movie.title === title);
     console.log(typeof(titulosPeliculas));
     console.table(titulosPeliculas);
-    return titulosPeliculas[3].people.map(personaje => personaje.name);
+    return titulosPeliculas; //.poster.map(personaje => personaje.name);
 
 }
-console.log(filtrarNombres("Hayao Miyazaki"));
+*/
+//console.log(filtrarNombres("Hayao Miyazaki"));
 
 
+//función para generar tarjetas de peliculas dinamicamente
+//el parametro peliculas se encuentra anteriormente como constante
 
-const btn = document.querySelector('#btnNav');
-btn.addEventListener ('click', () => {
+const items = document.getElementById('items');
+const templateCard = document.getElementById('template-card').content;
+const fragment = document.createDocumentFragment();
+
+
+const mostrarcard = peliculas => {
+    peliculas.forEach(pelicula => {
+        templateCard.querySelector('h5').textContent = pelicula.title
+        templateCard.querySelector('img').setAttribute('src', pelicula.poster)
+        const clone = templateCard.cloneNode(true)
+
+        fragment.appendChild(clone)
+    });
+    items.appendChild(fragment);
+};
+mostrarcard(peliculas);
+
+
+//ventana modal desplegada del boton 'sobre el estudio' en index.html
+
+const btn = document.getElementById("btnNav");
+console.log(btn);
+btn.addEventListener('click', () => {
+    console.log('hola');
     if (document.getElementById('btnNav')) {
         let modal = document.getElementById('VerModal');
         let span = document.getElementsByClassName('close')[0];
@@ -59,4 +81,4 @@ btn.addEventListener ('click', () => {
         } 
     }
 }
-)
+);
